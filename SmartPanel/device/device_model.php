9c317ca893f99d974a1f2b37802b3bff68a474cd
David Vargas <dvcarrillo@correo.ugr.es>
2017-04-07 20:35:02 +0300

    Added more functions and classes

--- SmartPanel/device/device_model.php
+++ SmartPanel/device/device_model.php
@@ -12,7 +12,31 @@ Backend structure -->
 		function Device() {
 			$this->mbd=Connector::DBConnection();
 		}
-	}
 
-	
+		// Search for the given ID in the device table 
+		public function getID($id) {
+			$sentence = $this->mbd->prepare("SELECT id FROM device WHERE id=".$id);
+			$sentence->execute();
+			$row = $sentence->fetch();
+			return $row[0];
+		}
+
+		// Get a list with all the IDs in device table
+		public function getListID() {
+			$sentence = "SELECT id FROM device";
+			foreach ($this->mbd->query($sentence) as $row) {
+				echo $row['id'];
+				echo " ";
+			}
+		}
+
+		// List all the devices' ID in the desired room
+		public function getIDInRoom($roomID) {
+			$sentence = "SELECT id FROM device WHERE RoomID=".$roomID;
+			foreach ($this->mbd->query($sentence) as $row) {
+				echo $row['id'];
+				echo " ";
+			}
+		}
+	}
 ?>
